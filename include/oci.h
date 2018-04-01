c03312659524a29aa81908c0b4fb96894abbdc2d
Corundex <24685043+Corundex@users.noreply.github.com>
2018-03-31 23:49:13 -0400

    Update oci.h

--- include/oci.h
+++ include/oci.h
@@ -1,1 +1,12 @@
-// empty
+// minimal set
+
+typedef   signed int     sword;
+typedef unsigned char    text;
+
+typedef unsigned char    ub1;
+typedef unsigned short   ub2;
+typedef unsigned long    ub4;
+
+typedef   signed short   sb2;
+typedef   signed long    sb4;
+typedef   signed __int64 sb8;
