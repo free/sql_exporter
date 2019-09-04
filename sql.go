4cbf754597e24127f14af532dd28d0dffafd3e41
Michael Glaesemann <grzm@seespotcode.net>
2019-09-04 13:51:48 -0500

    Add Vertica driver.

--- sql.go
+++ sql.go
@@ -9,8 +9,9 @@ import (
 	_ "github.com/denisenkom/go-mssqldb" // register the MS-SQL driver
 	_ "github.com/go-sql-driver/mysql"   // register the MySQL driver
 	log "github.com/golang/glog"
-	_ "github.com/kshvakov/clickhouse" // register the ClickHouse driver
-	_ "github.com/lib/pq"              // register the PostgreSQL driver
+	_ "github.com/kshvakov/clickhouse"    // register the ClickHouse driver
+	_ "github.com/lib/pq"                 // register the PostgreSQL driver
+	_ "github.com/vertica/vertica-sql-go" // register the Vertica driver
 )
 
 // OpenConnection extracts the driver name from the DSN (expected as the URI scheme), adjusts it where necessary (e.g.
